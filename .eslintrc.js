c6fed05721342b7a2ddaec3a7b2ae6be4b6bd7b6
fullstackmommy <nicole.budiman@thoughtworks.com>
2020-05-09 10:11:54 +0800

    :recycle: Replace Carousel state with HasIndex HOC

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,5 @@
 module.exports = {
+  plugins: ["react"],
   extends: ["eslint:recommended", "plugin:react/recommended"],
   parserOptions: {
     ecmaVersion: 6,
@@ -12,7 +13,8 @@ module.exports = {
   },
   rules: {
     quotes: ["error", "single", { avoidEscape: true }],
-    "comma-dangle": ["error", "always-multiline"]
+    "comma-dangle": ["error", "always-multiline"],
+    "no-unused-vars": ["error", { varsIgnorePattern: "^_" }]
   },
   settings: {
     react: {
