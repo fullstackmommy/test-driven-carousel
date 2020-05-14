b816ac0faeeb7588174b1ef7b9dc2a9b21c47239
fullstackmommy <nicole.budiman@thoughtworks.com>
2020-05-14 20:02:23 +0800

    :wrench: update deprecated jest config

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  setupTestFrameworkScriptFile: './src/tests/jestSetup.js',
+  setupFilesAfterEnv: ['./src/tests/jestSetup.js'],
   snapshotSerializers: ['enzyme-to-json/serializer'],
 };
